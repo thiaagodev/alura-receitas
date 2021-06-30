37c18b2ba3d272b0b5e23edd2b689393a5ccc95e
Thiago Ferreira <thiaago.dev@gmail.com>
2021-06-30 20:17:54 -0300

    Página receitas, separando arquivos html com partials e extend

--- receitas/views.py
+++ receitas/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def receita(request):
+    return render(request, 'receita.html')
