334b2d07665a27a8a33d759c9b1137db822c5bd9
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-01 17:00:35 -0300

    Admin e mostrando dados do banco

--- receitas/views.py
+++ receitas/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Receita
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    receitas = Receita.objects.all()
+    dados = {
+        'receitas': receitas
+    }
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
