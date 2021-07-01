2aee1c02aa43c7428dd14b246bfd520f9e287d8c
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-01 17:41:17 -0300

    Modificando admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class LisReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'categoria', 'tempo_de_preparo' ,'data')
+    list_display_links = ('id', 'nome')
+
+admin.site.register(Receita, LisReceitas)
