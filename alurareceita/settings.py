1a33f90045af57b1e5a369dc9353aec8d33d67c9
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-01 10:32:49 -0300

    Ajustes

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'alura_receita',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '172.17.192.1',
-        'PORT': '5432'
+        'USER': os.getenv('USER_BD'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT')
     }
 }
 
