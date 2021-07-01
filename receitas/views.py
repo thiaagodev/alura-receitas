3ed98781ad73cc165f3e4764739ee46460be50ed
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-01 17:17:43 -0300

    Parâmetros na URL, exibindo detalhes da receita

--- receitas/views.py
+++ receitas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Receita
 
@@ -10,5 +10,9 @@ def index(request):
     }
     return render(request, 'index.html', dados)
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    dados = {
+        'receita': receita
+    }
+    return render(request, 'receita.html', dados)
