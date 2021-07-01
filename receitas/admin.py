a726036ec2663b8a475ef85f1a89282eba279600
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-01 17:50:51 -0300

    Mais algumas modificações, como filtros e paginação

--- receitas/admin.py
+++ receitas/admin.py
@@ -4,5 +4,8 @@ from .models import Receita
 class LisReceitas(admin.ModelAdmin):
     list_display = ('id', 'nome', 'categoria', 'tempo_de_preparo' ,'data')
     list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_per_page = 10
 
 admin.site.register(Receita, LisReceitas)
