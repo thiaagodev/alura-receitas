c252b3121498c5bd646d40cb71090b83189b5173
Thiago Ferreira <thiaago.dev@gmail.com>
2021-06-30 21:48:31 -0300

    Configurando postgresql

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': '172.17.192.1',
+        'PORT': '5432'
     }
 }
 
