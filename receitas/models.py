c66b30699037f04ec5d7619c22d509f2dfa6c974
Thiago Ferreira <thiaago.dev@gmail.com>
2021-06-30 22:01:06 -0300

    Model de receitas

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import TextField
 
-# Create your models here.
+class Receita(models.Model):
+    nome = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_de_preparo = models.IntegerField()
+    rendimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    data = models.DateField(auto_now_add=True)
