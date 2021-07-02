e6890ed218685c6d8a7e51b400f30dc8ffa38ed5
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-02 17:50:03 -0300

    Autorização e refatorando o código

--- receitas/models.py
+++ receitas/models.py
@@ -14,3 +14,5 @@ class Receita(models.Model):
     foto = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
     publicada = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.nome
