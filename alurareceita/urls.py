187c1ff480107cc4b4f69ac19ee981ae5b1fb107
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-02 18:18:19 -0300

    app de usuarios

--- alurareceita/urls.py
+++ alurareceita/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('receitas.urls')),
     path('admin/', admin.site.urls),
+    path('', include('receitas.urls')),
+    path('usuarios/', include('usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
