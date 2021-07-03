c386f9edb385c196d0abdcea42185c6cd52ed9ac
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-03 17:04:23 -0300

    Realizando o login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == 'POST':
@@ -33,10 +34,26 @@ def cadastro(request):
         return render(request, 'usuarios/cadastro.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == '' or senha == '':
+            print('Os campos email e senha não podem ficar em branco!')
+            return redirect('login')
+        
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso!')
+                return redirect('dashboard')
+                
     return render(request, 'usuarios/login.html')
 
 def logout(request):
     pass
 
 def dashboard(request):
-    pass
+    return render(request, 'usuarios/dashboard.html')
