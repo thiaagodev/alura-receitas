3aa0ecbdc1d63a9b85408b30bc199d418d006848
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-05 18:23:51 -0300

    Refatorando o código

--- usuarios/views.py
+++ usuarios/views.py
@@ -10,19 +10,23 @@ def cadastro(request):
         senha = request.POST['password']
         senha2 = request.POST['password2']
 
-        if not nome.strip():
+        if campo_vazio(nome):
             messages.error(request, 'O nome não pode ser em branco!')
             return redirect('cadastro')
 
-        if not email.strip():
+        if campo_vazio(email):
             messages.error(request, 'O email não pode ser em branco!')
             return redirect('cadastro')
 
-        if senha != senha2:
+        if senhas_nao_sao_iguais(senha, senha2):
             messages.error(request, 'As senhas não são iguais!')
             return redirect('cadastro')
 
         if User.objects.filter(email=email).exists():
+            messages.error(request, 'Usuário já cadastrado!')
+            return redirect('cadastro')
+
+        if User.objects.filter(username=nome).exists():
             messages.error(request, 'Usuário já cadastrado!')
             return redirect('cadastro')
         
@@ -38,7 +42,7 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         senha = request.POST['senha']
-        if email == '' or senha == '':
+        if campo_vazio(email) or campo_vazio(senha):
             messages.error(request, 'Os campos email e senha não podem ficar em branco!')
             return redirect('login')
         
@@ -87,3 +91,9 @@ def cria_receita(request):
         return redirect('dashboard')
     else:      
         return render(request, 'usuarios/cria_receita.html')
+
+def campo_vazio(campo):
+    return not campo.strip()
+
+def senhas_nao_sao_iguais(senha, senha2):
+    return senha != senha2
