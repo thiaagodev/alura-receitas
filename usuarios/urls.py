0f3da936fdef19b202c9b9413e477f47f829f068
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-05 18:49:33 -0300

    Deletando receitas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('cria/receita', views.cria_receita, name='cria_receita')
+    path('cria/receita', views.cria_receita, name='cria_receita'),
+    path('deleta/<int:receita_id>', views.deleta_receita, name='deleta_receita'),
 ] 
