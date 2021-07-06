658642ce089cacb05db7b52c41b74f9af11fb62f
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-06 17:18:43 -0300

    Refatorando as views

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,8 +8,4 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('cria/receita', views.cria_receita, name='cria_receita'),
-    path('deleta/<int:receita_id>', views.deleta_receita, name='deleta_receita'),
-    path('edita/<int:receita_id>', views.edita_receita, name='edita_receita'),
-    path('atualiza_receita', views.atualiza_receita, name='atualiza_receita'),
 ] 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from receitas.models import Receita
@@ -76,57 +76,6 @@ def dashboard(request):
         return render(request, 'usuarios/dashboard.html', dados)
     else:
         return redirect('index')
-
-def cria_receita(request):
-    if request.method == 'POST':
-        nome_receita = request.POST['nome_receita']
-        ingredientes = request.POST['ingredientes']
-        modo_preparo = request.POST['modo_preparo']
-        tempo_preparo = request.POST['tempo_preparo']
-        rendimento = request.POST['rendimento']
-        categoria = request.POST['categoria']
-        foto_receita = request.FILES['foto_receita']
-        user = get_object_or_404(User, pk=request.user.id)
-
-        receita = Receita.objects.create(pessoa=user, nome=nome_receita, ingredientes=ingredientes, modo_preparo=modo_preparo, tempo_de_preparo=tempo_preparo, rendimento=rendimento, categoria=categoria, foto=foto_receita)
-        receita.save()
-
-        return redirect('dashboard')
-    else:      
-        return render(request, 'receitas/cria_receita.html')
-
-def deleta_receita(request, receita_id):
-    receita = get_object_or_404(Receita, pk=receita_id)
-    receita.delete()
-    messages.success(request, f'A receita {receita.nome} foi delatada com sucesso!')
-    return redirect('dashboard')
-
-def edita_receita(request, receita_id):
-    receita = get_object_or_404(Receita, pk=receita_id)
-    receita_editar = {
-        'receita': receita
-    }
-   
-    return render(request, 'receitas/edita_receita.html', receita_editar)
-
-def atualiza_receita(request):
-    if request.method == 'POST':
-        receita_id = request.POST['receita_id']
-
-        receita = Receita.objects.get(pk=receita_id)
-        receita.nome = request.POST['nome_receita']
-        receita.ingredientes = request.POST['ingredientes']
-        receita.modo_preparo = request.POST['modo_preparo']
-        receita.tempo_de_preparo = request.POST['tempo_preparo']
-        receita.rendimento = request.POST['rendimento']
-        receita.categoria = request.POST['categoria']
-
-        if 'foto_receita' in request.FILES:
-            receita.foto = request.FILES['foto_receita']
-        
-        receita.save()
-        messages.success(request, f'Receita {receita.nome} atualizada com sucesso!')
-        return redirect('dashboard')
 
 def campo_vazio(campo):
     return not campo.strip()
